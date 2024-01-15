9c3731e80c88acddf030d30c1c4347d1e0502b08
Himanshu <himanshujain5690174@gmail.com>
2024-01-15 22:49:59 +0530

    orderPage

--- Frontend/src/components/user/MyOrders.jsx
+++ Frontend/src/components/user/MyOrders.jsx
@@ -1,33 +1,26 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
+import Order from '../productTemplates/Order';
 
 function MyOrders() {
     const { isLoggedIn } = useContext(AppContext);
-    console.log(isLoggedIn);
+    console.log("LogediN : ", isLoggedIn);
 
     return (
         <div>
-            <div>
+            <div className='w-10/12 bg-red-100 mx-auto flex justify-center items-start '>
                 <div>
                     {
                         isLoggedIn?.orders?.map((order) => (
-                            <div
-                                key={order?._id}
-                            >
-                                <div
-                                    className=''
-                                >
-                                    ji
-                                </div>
-                            </div>
+                            <Order order={order} key={order?._id} />
                         ))
                     }
-
                 </div>
+
             </div>
 
         </div>
     )
 }
 
-export default MyOrders
+export default MyOrders;
